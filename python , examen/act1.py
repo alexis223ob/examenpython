eee59a63ffc5705aa26cce06c334f8de97e3953f
alexis223ob <131316054+alexis223ob@users.noreply.github.com>
2024-04-08 12:55:42 -0300

    Add files via upload

---
+++ python , examen/act1.py
@@ -0,0 +1,13 @@
+import random;
+
+filas = 0
+for f in range(1):
+    filas = int(input("Ingrese la cantidad de filas: "))
+    if filas < 1:
+        input("La cantidad de filas tiene que ser mayor o igual a 1, porfavor ingrese otro nro : ")
+
+columnas = 0
+for c in range(1):
+    columnas = int(input("Ingrese la cantidad de columnas: "))
+    if columnas < 1:
+        input("La cantidad de columnas tiene que ser mayor o igual a 1, porfavor ingrese otro nro : ")
